59f3b71903a8dd7ef40efaad2290e69ab8cba7db
Minji Choi <comg2843@gmail.com>
2022-10-20 18:31:32 +0300

    Added addProduct submit action

--- client/src/pages/AddProduct.tsx
+++ client/src/pages/AddProduct.tsx
@@ -1,7 +1,29 @@
 import ProductForm from 'components/ProductForm';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from 'redux/hooks';
+import { addProduct } from 'redux/slices/productsSlice';
+import { ProductInput } from 'utils/types';
 
 const AddProduct = () => {
-  return <ProductForm />;
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const addAction = async (input: ProductInput) => {
+    const resultAction = await dispatch(addProduct(input));
+    if (addProduct.fulfilled.match(resultAction)) {
+      alert('Product added! ✨🛍');
+      navigate(-1);
+    }
+  };
+
+  return (
+    <main style={{ marginBottom: '6rem' }}>
+      <h1 className="page__title">Add product</h1>
+      <section className="product-form__container">
+        <ProductForm editMode={false} submitAction={addAction} />
+      </section>
+    </main>
+  );
 };
 
 export default AddProduct;
