fc9a6c237e4320a564cd8e7cfda09c01e4acac84
Minji Choi <comg2843@gmail.com>
2023-03-03 02:33:44 +0200

    changed app listening place

--- api/src/server.ts
+++ api/src/server.ts
@@ -10,6 +10,15 @@ mongoose
   .connect(mongoUrl)
   .then(() => {
     logger.info('Connected to MongoDB')
+    // Start Express server
+    app.listen(app.get('port'), () => {
+      console.log(
+        '  App is running at http://localhost:%d in %s mode',
+        app.get('port'),
+        app.get('env')
+      )
+      console.log('  Press CTRL-C to stop\n')
+    })
   })
   .catch((err: Error) => {
     console.log(
@@ -25,13 +34,3 @@ mongoose
 if (process.env.NODE_ENV === 'development') {
   app.use(errorHandler())
 }
-
-// Start Express server
-app.listen(app.get('port'), () => {
-  console.log(
-    '  App is running at http://localhost:%d in %s mode',
-    app.get('port'),
-    app.get('env')
-  )
-  console.log('  Press CTRL-C to stop\n')
-})
